bb5209c568d848eaf6a6620e06483d32a801d896
fuyuooumi1027 <fuyuooumi1027@gmail.com>
2020-01-03 21:31:09 +0900

    modify (iotHub): modify config

--- iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
+++ iot-hub/Quickstarts/read-d2c-messages/ReadDeviceToCloudMessages.js
@@ -14,7 +14,8 @@
 //
 // Using the Azure CLI:
 // az iot hub show-connection-string --hub-name {YourIoTHubName} --policy-name service  --output table
-var connectionString = '{Your service connection string here}';
+var config = require('../../config');
+var serviceConnectionString = config.iotHubServiceConnectionString;
 
 // Using the Node.js SDK for Azure Event hubs:
 //   https://github.com/Azure/azure-event-hubs-node
@@ -22,7 +23,7 @@ var connectionString = '{Your service connection string here}';
 // to read messages sent from a device.
 var { EventHubClient, EventPosition } = require('@azure/event-hubs');
 
-var printError = function (err) {
+var printError = function(err) {
   console.log(err.message);
 };
 
@@ -30,12 +31,12 @@ var printError = function (err) {
 // - Telemetry is sent in the message body
 // - The device can add arbitrary application properties to the message
 // - IoT Hub adds system properties, such as Device Id, to the message.
-var printMessage = function (message) {
+var printMessage = function(message) {
   console.log('Telemetry received: ');
   console.log(JSON.stringify(message.body));
-  console.log('Application properties (set by device): ')
+  console.log('Application properties (set by device): ');
   console.log(JSON.stringify(message.applicationProperties));
-  console.log('System properties (set by IoT Hub): ')
+  console.log('System properties (set by IoT Hub): ');
   console.log(JSON.stringify(message.annotations));
   console.log('');
 };
@@ -43,13 +44,20 @@ var printMessage = function (message) {
 // Connect to the partitions on the IoT Hub's Event Hubs-compatible endpoint.
 // This example only reads messages sent after this application started.
 var ehClient;
-EventHubClient.createFromIotHubConnectionString(connectionString).then(function (client) {
-  console.log("Successfully created the EventHub Client from iothub connection string.");
-  ehClient = client;
-  return ehClient.getPartitionIds();
-}).then(function (ids) {
-  console.log("The partition ids are: ", ids);
-  return ids.map(function (id) {
-    return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
-  });
-}).catch(printError);
+EventHubClient.createFromIotHubConnectionString(serviceConnectionString)
+  .then(function(client) {
+    console.log(
+      'Successfully created the EventHub Client from iothub connection string.'
+    );
+    ehClient = client;
+    return ehClient.getPartitionIds();
+  })
+  .then(function(ids) {
+    console.log('The partition ids are: ', ids);
+    return ids.map(function(id) {
+      return ehClient.receive(id, printMessage, printError, {
+        eventPosition: EventPosition.fromEnqueuedTime(Date.now())
+      });
+    });
+  })
+  .catch(printError);
